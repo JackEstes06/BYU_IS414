704bb53d9f300f7c669876a51821d5e2071d3f74
Jack Estes <jackestes@Jacks-MacBook-Pro-6.local>
2025-03-27 19:32:56 -0600

    Bullshit that doesn't work

--- Cryptography/Cryptography/obj/Debug/net8.0/Cryptography.AssemblyInfo.cs
+++ Cryptography/Cryptography/obj/Debug/net8.0/Cryptography.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cryptography")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a869363c4c7912d3a027bdb6db816f446be7022")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cryptography")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cryptography")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
